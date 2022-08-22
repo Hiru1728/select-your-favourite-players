0536cf199cf83c4046019629e56f9a7a94faab7d
unknown <Hiru1728>
2022-08-22 12:42:58 +0600

    add budget and tototal expense

---
+++ js/budget.js
@@ -0,0 +1,43 @@
+// event heandler using calculate button
+document.getElementById('btn-calculate').addEventListener('click', function () {
+    const perPlayerPriceElement = document.getElementById('per-player');
+    const perPlayerPrice = perPlayerPriceElement.value;
+    const totalPlayerPrice = perPlayerPrice * 5;
+
+    // Clear the input value
+    perPlayerPriceElement.value = '';
+
+    const playerExpensesElement = document.getElementById('player-expenses');
+    const playerExpensesElementString = playerExpensesElement.innerText;
+    const playerExpenses = parseFloat(playerExpensesElementString)
+    playerExpensesElement.innerText = totalPlayerPrice;
+
+})
+document.getElementById('calculate-total').addEventListener('click', function () {
+    const playerExpensesElement = document.getElementById('player-expenses');
+    const playerExpensesElementString = playerExpensesElement.innerText;
+    const playerExpenses = parseFloat(playerExpensesElementString);
+
+
+    const managerSalaryElement = document.getElementById('manager-salary');
+    const managerSalaryString = managerSalaryElement.value;
+    const managerSalary = parseFloat(managerSalaryString);
+    managerSalaryElement.value = '';
+
+
+    const coachSalaryElement = document.getElementById('coach-salary');
+    const coachSalaryString = coachSalaryElement.value;
+    const coachSalary = parseFloat(coachSalaryString);
+    coachSalaryElement.value = '';
+
+
+    let totalSalary = playerExpenses + managerSalary + coachSalary;
+
+    const totalExpensesElement = document.getElementById('total-expense');
+    const totalExpensesString = totalExpensesElement.innerText;
+    const totalExpense = parseFloat(totalExpensesString);
+    totalExpensesElement.innerText = totalSalary;
+
+
+
+})
